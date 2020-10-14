f563045c89c3da8b01570ea5cc94c16a456497f5
y-hashida <yoshimasa.hashida@link-u.co.jp>
2020-10-14 15:59:46 +0900

    update: molecule のシナリオを環境変数 MOLECULE_SCENARIO で指定できるようにした.

---
+++ scripts/molecule_test.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import os
+    import subprocess
+    import sys
+
+    ## python script path and repo dir
+    script_dir  = os.path.dirname(os.path.abspath(__file__))
+    molecule_py = os.path.abspath(script_dir + "/molecule.py")
+
+    ## 環境変数の取得
+    env = os.environ
+    molecule_scenario = env.get("MOLECULE_SCENARIO")
+
+    ## molecule.py を実行する
+    if molecule_scenario == None:
+        args = [molecule_py, "test", "--all"]
+    else:
+        args = [molecule_py, "test", "-s", molecule_scenario]
+
+    print(" ".join(args))
+    proc = subprocess.run(args, env = env)
+    sys.exit(proc.returncode)
